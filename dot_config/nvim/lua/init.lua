e5bdf799c038ba76e37c53707e51997437473eb5
arsmoriendy <nugrahasteven@gmail.com>
2023-05-09 17:42:08 +0700

    refactor(nvim): init.lua vim.keymap.set -> kms

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -13,7 +13,7 @@ singular mode and non-logical keymaps will be stored in init.vim ]]
 local kms = vim.keymap.set
 
 -- turn off search highlight until next search action (i.e. new search, next search, prev search)
-vim.keymap.set({"n", "i", "x"}, "<C-f>", vim.cmd.nohlsearch)
+kms({"n", "i", "x"}, "<C-f>", vim.cmd.nohlsearch)
 
 -- tab navigation [[
 kms({"n", "i", "x"}, "<C-Tab>", vim.cmd.tabnext)
@@ -29,7 +29,7 @@ kms({"n", "i", "x"}, "<C-M-r>", "<CMD>later 1f<CR>")
 local luasnip = require("luasnip")
 
 -- jump forwards in snippets / tab
-vim.keymap.set({"i", "s"}, "<Tab>", function()
+kms({"i", "s"}, "<Tab>", function()
   require('lualine').refresh()
   if luasnip.locally_jumpable() then
     return "<Plug>luasnip-jump-next"
@@ -39,7 +39,7 @@ vim.keymap.set({"i", "s"}, "<Tab>", function()
 end, {silent = true, expr = true})
 
 -- jump backwards in snippets
-vim.keymap.set({"i", "s"}, "<S-Tab>", function()
+kms({"i", "s"}, "<S-Tab>", function()
   if luasnip.jumpable() then
     luasnip.jump(-1)
   end

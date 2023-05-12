b340fd5a14b801edfe7c7d2c4a5872161794bbe7
arsmoriendy <nugrahasteven@gmail.com>
2023-05-12 16:13:39 +0700

    feat(nvim): hide lualine in alpha

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -96,7 +96,12 @@ require("lazy").setup({
       require("lualine").setup({
         options = {
           section_separators = {left = "", right = ""},
-          component_separators = {left = "╲", right = "╱"}
+          component_separators = {left = "╲", right = "╱"},
+          disabled_filetypes = {
+            statusline = {
+              "alpha",
+            },
+          },
         },
         sections = {
           lualine_a = {

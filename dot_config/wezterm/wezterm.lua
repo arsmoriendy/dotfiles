ca0b117f7aa8c409042d4f7e4783286b34f80494
arsmoriendy <nugrahasteven@gmail.com>
2023-11-11 16:07:23 +0700

    feat(wezterm): hide tab bar, tab bar size

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -18,11 +18,11 @@ config.color_scheme = "GruvboxDarkHard"
 
 config.window_background_opacity = 0.9
 
--- config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_frame = {
   font = wezterm.font("CaskaydiaCove Nerd Font"),
-  font_size = 10,
+  font_size = 11,
 }
 
 config.colors = {

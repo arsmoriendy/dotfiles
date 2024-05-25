b50cce1be19327a40a5eda65a4970f8b6f4b6881
arsmoriendy <nugrahasteven@gmail.com>
2024-05-25 16:01:17 +0700

    fix(nvim): ibl indent char
    `ibl`: indent-blankline plugin
    
    Use thinner line character for ibl
    
    Also:
    - Removed possible mistake `indent.tab_char` ibl setting
    - Removed redundant `scope.highlight` ibl setting
    - Removed `exclude` ibl setting, since neorg is no longer used

--- dot_config/nvim/lua/plugins/indent-blankline-nvim.lua
+++ dot_config/nvim/lua/plugins/indent-blankline-nvim.lua
@@ -5,29 +5,8 @@ return {
   config = function()
     vim.cmd.highlight({ "IblScope", "guifg=#fb4934" })
     require("ibl").setup({
-      scope = {
-        highlight = {
-          "IblScope"
-        }
-      },
-      exclude = {
-        filetypes = {
-          -- defaults
-          'lspinfo',
-          "packer",
-          "checkhealth",
-          "help",
-          "man",
-          "gitcommit",
-          "TelescopePrompt",
-          "TelescopeResults",
-          "''",
-          -- custom
-          "norg",
-        }
-      },
       indent = {
-        tab_char = "▎"
+        char = "▏",
       },
     })
   end

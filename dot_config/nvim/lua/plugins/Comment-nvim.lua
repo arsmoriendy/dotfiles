6ec1c6b9f284160c3c56d59534228166de1b7b44
arsmoriendy <nugrahasteven@gmail.com>
2024-05-26 08:41:02 +0700

    perf(nvim): load Comment.nvim VeryLazy
    Also:
    - Added docs for native commenting in Comment.nvim config file
    - Removed `kdl` filetype setting for Comment.nvim

--- dot_config/nvim/lua/plugins/Comment-nvim.lua
+++ dot_config/nvim/lua/plugins/Comment-nvim.lua
@@ -1,8 +1,16 @@
 return {
-  "numToStr/Comment.nvim",     -- commenting helper
+  "numToStr/Comment.nvim", -- commenting helper
+  event = "VeryLazy",
+  -- From neovim version 0.10.0 onwards, commenting is native.
+  -- Therefore, this plugin shouldn't be needed.
+  -- But for now `gcA` and `gco` maps are not natively supported.
+  -- So this plugin is still used.
+  -- Uncomment `cond` below to disable this plugin on neovim >= 0.10.0.
+  -- cond = function()
+  --   local v = vim.version()
+  --   return v.major <= 0 and v.minor < 10
+  -- end,
   config = function()
     require("Comment").setup()
-    local ft = require("Comment.ft")
-    ft.kdl = { "// %s" }
   end
 }

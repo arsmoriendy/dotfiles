b130b50467bd810748bd7010ec31b85e7b226797
arsmoriendy <nugrahasteven@gmail.com>
2024-06-01 11:37:01 +0700

    feat(nvim): no scrollbar for prompt and dressing

--- dot_config/nvim/lua/plugins/nvim-scrollbar.lua
+++ dot_config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -1,18 +1,21 @@
 return {
-  "petertriho/nvim-scrollbar",       -- scrollbar
+  "petertriho/nvim-scrollbar",   -- scrollbar
   dependencies = {
-    "kevinhwang91/nvim-hlslens",     -- search handler
-    "lewis6991/gitsigns.nvim"        -- git signs handler
+    "kevinhwang91/nvim-hlslens", -- search handler
+    "lewis6991/gitsigns.nvim"    -- git signs handler
   },
   config = function()
-    require("scrollbar.handlers.search").setup({})     -- need table parameter
+    require("scrollbar.handlers.search").setup({}) -- need table parameter
     require("scrollbar.handlers.gitsigns").setup()
     require("scrollbar").setup({
       hide_if_all_visible = false,
+      excluded_buftypes = {
+        "prompt", -- (e.g. telescope)
+      },
       excluded_filetypes = {
-        -- disable scrollbar for alpha (blank startup plugin)
         "alpha",
-        "ccc-ui",
+        "ccc-ui", -- colorpicker
+        "DressingInput",
       },
       handle = {
         highlight = "Visual"

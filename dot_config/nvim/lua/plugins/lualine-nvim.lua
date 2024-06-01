332d3741471835ad3e2748f2a7c6be3037fdbc89
arsmoriendy <nugrahasteven@gmail.com>
2024-06-01 19:53:08 +0700

    feat(nvim): lualine tabline icon color

--- dot_config/nvim/lua/plugins/lualine-nvim.lua
+++ dot_config/nvim/lua/plugins/lualine-nvim.lua
@@ -1,5 +1,6 @@
 -- TODO: lualine_b separator color
--- TODO: tabline icon color
+-- TODO: default tabline filetype icon light color (nvim-web-devicons)
+-- TODO: derive statusline filetype icon from filetype and not file extension
 
 return {
   "nvim-lualine/lualine.nvim", -- statusline
@@ -13,6 +14,7 @@ return {
     local navic = require("nvim-navic")
     local notify = require("notify")
     local nwd = require("nvim-web-devicons")
+    local nwd_light = require("nvim-web-devicons.icons-light").icons_by_file_extension
 
     -- variables used to match active and inactive (global) [
     local sections = {
@@ -100,7 +102,13 @@ return {
         lualine_a = {
           {
             "tabs",
-            max_length = vim.o.columns,
+            -- HACK: Set to really big number because highlight escape
+            -- characters adds to the overall length of each tab.
+            -- TODO: Fix horizontal scroll when tabline overflows.
+            -- [
+            tab_max_length = 999,
+            max_length = 999,
+            -- ]
             mode = 1,
             show_modified_status = false,
             fmt = function(name, context)
@@ -108,14 +116,35 @@ return {
               local winnr = vim.fn.tabpagewinnr(context.tabnr)
               local bufnr = buflist[winnr]
 
+              local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+              local ext = nwd.get_icon_name_by_filetype(ft)
+              local icon_tbl = nwd_light[ext] or nwd.get_default_icon()
+
+              local tab_icon_hi = "lualine_tab" .. context.tabnr .. "_icon"
+
+              local is_curr = vim.api.nvim_get_current_tabpage() == context.tabnr
+              local bg_hi = is_curr and "lualine_a_tabs_active" or "lualine_a_tabs_inactive"
+
+              local bg_c = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(bg_hi)), "bg#")
+
+              local icon = string.format(
+                "%%#%s#%s%%#%s#",
+                tab_icon_hi,
+                icon_tbl.icon,
+                bg_hi
+              )
+
               local is_modified = vim.fn.getbufvar(bufnr, "&modified")
 
-              local filetype_icon = nwd.get_icon(name)
-              local default_filetype_icon = ""
+              vim.cmd.highlight({
+                tab_icon_hi,
+                "guifg=" .. icon_tbl.color,
+                "guibg=" .. bg_c
+              })
 
               return string.format(
                 "%s %s%s",
-                filetype_icon or default_filetype_icon,
+                icon,
                 name,
                 is_modified == 1 and " ●" or ""
               )

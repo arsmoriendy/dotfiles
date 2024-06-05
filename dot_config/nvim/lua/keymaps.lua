5371a5b79959835c069cbba424e43d063f008dad
arsmoriendy <nugrahasteven@gmail.com>
2024-06-05 11:01:44 +0700

    feat(nvim): keymap descriptions for keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -4,41 +4,41 @@
 local kms = vim.keymap.set
 
 -- turn off search highlight until next search action (i.e. new search, next search, prev search)
-kms({ "n", "i", "x" }, "<C-f>", vim.cmd.nohlsearch)
+kms({ "n", "i", "x" }, "<C-f>", vim.cmd.nohlsearch, { desc = "Temporarily disable search highlights" })
 
 -- tab navigation [
-kms({ "n", "i", "x" }, "<C-Tab>", vim.cmd.tabnext)
-kms({ "n", "i", "x" }, "<C-S-Tab>", vim.cmd.tabprevious)
-kms({ "n", "i", "x" }, "<PageDown>", vim.cmd.tabnext)
-kms({ "n", "i", "x" }, "<PageUp>", vim.cmd.tabprevious)
+kms({ "n", "i", "x" }, "<C-Tab>", vim.cmd.tabnext, { desc = "Go to next tab" })
+kms({ "n", "i", "x" }, "<C-S-Tab>", vim.cmd.tabprevious, { desc = "Go to previous tab" })
+kms({ "n", "i", "x" }, "<PageDown>", vim.cmd.tabnext, { desc = "Go to next tab" })
+kms({ "n", "i", "x" }, "<PageUp>", vim.cmd.tabprevious, { desc = "Go to previous tab" })
 -- ]
 
 -- window resize [
-kms("n", "<C-w>h", "<CMD>vertical resize -5<CR>")
-kms("n", "<C-w>j", "<CMD>resize +5<CR>")
-kms("n", "<C-w>k", "<CMD>resize -5<CR>")
-kms("n", "<C-w>l", "<CMD>vertical resize +5<CR>")
+kms("n", "<C-w>h", "<CMD>vertical resize -5<CR>", { desc = "Reduce window size vertically" })
+kms("n", "<C-w>j", "<CMD>resize +5<CR>", { desc = "Add window size horizontally" })
+kms("n", "<C-w>k", "<CMD>resize -5<CR>", { desc = "Reduce window size horizontally" })
+kms("n", "<C-w>l", "<CMD>vertical resize +5<CR>", { desc = "Add window size vertically" })
 -- ]
 
 -- navigate saves [
-kms({ "n", "i", "x" }, "<C-M-u>", "<CMD>earlier 1f<CR>")
-kms({ "n", "i", "x" }, "<C-M-r>", "<CMD>later 1f<CR>")
+kms({ "n", "i", "x" }, "<C-M-u>", "<CMD>earlier 1f<CR>", { desc = "Undo to last save" })
+kms({ "n", "i", "x" }, "<C-M-r>", "<CMD>later 1f<CR>", { desc = "Redo to next save" })
 -- ]
 
-kms({ "n" }, "<Leader>f", "za") -- toggle fold
+kms({ "n" }, "<Leader>f", "za", { desc = "Toggle fold" })
 
 -- exit
-kms("n", "<S-q>", "<CMD>qa!<CR>")
+kms("n", "<S-q>", "<CMD>qa!<CR>", { desc = "Exit neovim without saving" })
 -- kms("n", "<C-w><C-q>", "<CMD>q!<CR>")
 
 -- save / write file [
-kms({ "n", "v", "o" }, "<C-s>", "<CMD>w<CR>")
+kms({ "n", "v", "o" }, "<C-s>", "<CMD>w<CR>", { desc = "Save file" })
 -- separate insert mode mapping for going back to normal mode after saving
-kms({ "i" }, "<C-s>", "<ESC><CMD>w<CR>")
+kms({ "i" }, "<Esc><C-s>", "<CMD>w<CR>", { desc = "Save file" })
 -- ]
 
 -- delete
-kms("i", "<C-l>", "<DEL>")
+kms("i", "<C-l>", "<DEL>", { desc = "Delete" })
 
 -- yank and append line to the unnamed register (:help registers)
 kms("n", "yY", "<CMD>let @\" .= getline('.') . \"\\n\"<CR>", {
@@ -54,35 +54,35 @@ kms("n", "dD", "<CMD>let @\" .= getline('.') . \"\\n\" | d _<CR>", {
 
 -- windows [
 -- navigate windows [
-kms("n", "<C-h>", "<C-w>h", { remap = false })
-kms("n", "<C-j>", "<C-w>j", { remap = false })
-kms("n", "<C-k>", "<C-w>k", { remap = false })
-kms("n", "<C-l>", "<C-w>l", { remap = false })
+kms("n", "<C-h>", "<C-w>h", { remap = false, desc = "Go to left window" })
+kms("n", "<C-j>", "<C-w>j", { remap = false, desc = "Go to down window" })
+kms("n", "<C-k>", "<C-w>k", { remap = false, desc = "Go to up window" })
+kms("n", "<C-l>", "<C-w>l", { remap = false, desc = "Go to right window" })
 -- ]
 -- close window
-kms({ "n", "v", "o" }, "<C-q>", "<C-w>q")
+kms({ "n", "v", "o" }, "<C-q>", "<C-w>q", { desc = "Close window" })
 -- ]
 
 -- diagnostics [
-kms("n", "<Enter>", "<CMD>lua vim.diagnostic.open_float()<CR>", { silent = true })
-kms("n", "<Tab>", "<CMD>lua vim.diagnostic.goto_next()<CR>", { silent = true })
-kms("n", "<S-Tab>", "<CMD>lua vim.diagnostic.goto_prev()<CR>", { silent = true })
+kms("n", "<Enter>", "<CMD>lua vim.diagnostic.open_float()<CR>", { silent = true, desc = "Show diagnostic window" })
+kms("n", "<Tab>", "<CMD>lua vim.diagnostic.goto_next()<CR>", { silent = true, desc = "Go to next diagnostic" })
+kms("n", "<S-Tab>", "<CMD>lua vim.diagnostic.goto_prev()<CR>", { silent = true, desc = "Go to previous diagnostic" })
 -- ]
 
 -- lsp rename
-kms("n", "<Leader>r", "<CMD>lua vim.lsp.buf.rename()<CR>")
+kms("n", "<Leader>r", "<CMD>lua vim.lsp.buf.rename()<CR>", { desc = "Rename symbol" })
 
 -- lsp format
-kms("n", "<Leader>i", "<CMD>lua vim.lsp.buf.format()<CR>")
+kms("n", "<Leader>i", "<CMD>lua vim.lsp.buf.format()<CR>", { desc = "Reformat buffer" })
 
 -- lsp hover
-kms("n", "<Leader>k", "<CMD>lua vim.lsp.buf.hover()<CR>")
+kms("n", "<Leader>k", "<CMD>lua vim.lsp.buf.hover()<CR>", { desc = "Simulate hover symbol" })
 
 -- lsp go to definition
-kms("n", "<Leader>d", "<CMD>lua vim.lsp.buf.definition()<CR>");
+kms("n", "<Leader>d", "<CMD>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" });
 
 -- lsp action
-kms("n", "<Leader>a", "<CMD>lua vim.lsp.buf.code_action()<CR>");
+kms("n", "<Leader>a", "<CMD>lua vim.lsp.buf.code_action()<CR>", { desc = "Select lsp actions" });
 
 kms("n", "==", "<CMD>mkview<CR>gg=G<CMD>loadview<CR>", { desc = "Reindent all lines" })
 

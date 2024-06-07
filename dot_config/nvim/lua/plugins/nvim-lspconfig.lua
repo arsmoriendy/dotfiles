c59ae8af710d7318dc5fd787aed03a4952e437d0
arsmoriendy <nugrahasteven@gmail.com>
2024-06-07 10:52:39 +0700

    fix(nvim): proper lspconfig overrides

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,47 +1,54 @@
 local function config()
-  local function defaultSetupHandler(server_name)
-    -- specific server configs
-    local configs = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { "vim" },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-          }
-        }
-      },
-      emmet_ls = {
-        -- add php for emmet
-        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "eruby", "php" },
-      },
-      intelephense = {
-        telemetry = {
-          enabled = false,
-        },
-      },
-    };
-    -- current config
-    local srv_config = configs[server_name] or {};
-    -- append default configs
-    srv_config.capabilities = require("cmp_nvim_lsp").default_capabilities(); -- cmp lsp capabilities
-    srv_config.on_attach = function(client, bufnr)                            -- attach nvim-navic if possible
+  local lspconfig = require("lspconfig")
+  local lib = require("lib")
+  local bind = lib.bind
+
+  -- default server overrides [
+  local default_lspconfig_overrides = {
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    on_attach = function(client, bufnr) -- attach nvim-navic if possible
       if client.server_capabilities.documentSymbolProvider then
         require("nvim-navic").attach(client, bufnr)
       end
-    end;
-    require("lspconfig")[server_name].setup(srv_config);
-  end
+    end
+  }
+
+  lspconfig.util.default_config = vim.tbl_extend("force",
+    lspconfig.util.default_config, default_lspconfig_overrides)
+  -- ]
+
+  -- specific server overrides [
+  local lua_ls_cfg = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      }
+    }
+  }
+
+  local emmet_ls_cfg = {
+    -- add php for emmet
+    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "eruby", "php" },
+  }
+
+  local intelephense_cfg = {
+    telemetry = {
+      enabled = false,
+    },
+  }
+  -- ]
 
   -- dependency ordering matters
   require("mason").setup({
@@ -52,7 +59,9 @@ local function config()
   require("mason-lspconfig").setup({})
   -- automatic server config setup (:h mason-lspconfig-automatic-server-setup)
   require("mason-lspconfig").setup_handlers({
-    defaultSetupHandler
+    ["lua_ls"] = bind(lspconfig["lua_ls"].setup, lua_ls_cfg),
+    ["emmet_ls"] = bind(lspconfig["emmet_ls"].setup, emmet_ls_cfg),
+    ["intelephense"] = bind(lspconfig["intelephense"].setup, intelephense_cfg),
   })
 
   vim.keymap.set({ "n" }, "<Leader>m", "<Cmd>Mason<CR>", { desc = "Open Mason ui" })

9be173217c7f3b076ba4cbf023ac341e19b130ae
arsmoriendy <nugrahasteven@gmail.com>
2024-06-19 16:01:23 +0700

    feat(nvim): sync ibl indent char with tab listchar

--- dot_config/nvim/lua/plugins/indent-blankline-nvim.lua
+++ dot_config/nvim/lua/plugins/indent-blankline-nvim.lua
@@ -1,13 +1,15 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",     -- indent lines
+  "lukas-reineke/indent-blankline.nvim", -- indent lines
   dependencies = "ellisonleao/gruvbox.nvim",
   main = "ibl",
   config = function()
+    local char = "▏"
     vim.cmd.highlight({ "IblScope", "guifg=#fb4934" })
     require("ibl").setup({
       indent = {
-        char = "▏",
+        char = char,
       },
     })
+    vim.opt.listchars:append(string.format("tab:%s ", char))
   end
 }

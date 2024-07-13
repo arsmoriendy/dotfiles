2607ec7bddc0d0edb00f38febe500002de40bb6a
arsmoriendy <nugrahasteven@gmail.com>
2024-07-13 12:18:14 +0700

    feat(nvim)!: disabled which-key-nvim

--- dot_config/nvim/lua/plugins/which-key-nvim.lua
+++ dot_config/nvim/lua/plugins/which-key-nvim.lua
@@ -1,5 +1,12 @@
+-- TODO: Passthrough <C-c> (and possibly other mappings)
+-- E.g. The sequence `vg` -> wait for popup -> `<C-c>`,
+--      will only close the popup,
+--      but should also cancel the sequence,
+--      the same way it works when not waiting for the popup.
+-- TODO: Re-enable when `v%` works as intended
 return {
   "folke/which-key.nvim",
+  enabled = false,
   event = "VeryLazy",
   opts = {
     icons = {

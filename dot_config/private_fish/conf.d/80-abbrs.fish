3d82347274648fe6e68179b6349d23b062185405
arsmoriendy <nugrahasteven@gmail.com>
2024-08-14 11:57:07 +0700

    feat(fish): "la" and "lam" abbr to show groups

--- dot_config/private_fish/conf.d/80-abbrs.fish
+++ dot_config/private_fish/conf.d/80-abbrs.fish
@@ -1,8 +1,8 @@
 abbr -a p "sudo $PACK_MAN"
 abbr -a sctl "sudo systemctl"
 abbr -a ls "eza --icons"
-abbr -a la "eza -AlH --icons"
-abbr -a lam "eza -AlH --icons -s modified --time-style long-iso"
+abbr -a la "eza -AlHg --icons"
+abbr -a lam "eza -AlHg --icons -s modified --time-style long-iso"
 abbr -a che "chezmoi"
 # fzf modified file in current directory and merge it
 abbr -a chem "chezmoi merge (chezmoi --path-style absolute status . | awk '{print \$2}' | fzf)"

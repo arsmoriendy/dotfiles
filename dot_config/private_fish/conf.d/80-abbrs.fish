16068f54b59c75a700083fd4a89da75e6796a15c
arsmoriendy <nugrahasteven@gmail.com>
2024-08-14 11:56:01 +0700

    fix(fish): redundant "la" and "lam" abbr -F flag

--- dot_config/private_fish/conf.d/80-abbrs.fish
+++ dot_config/private_fish/conf.d/80-abbrs.fish
@@ -1,8 +1,8 @@
 abbr -a p "sudo $PACK_MAN"
 abbr -a sctl "sudo systemctl"
 abbr -a ls "eza --icons"
-abbr -a la "eza -AlH --icons -F"
-abbr -a lam "eza -AlH --icons -F -s modified --time-style long-iso"
+abbr -a la "eza -AlH --icons"
+abbr -a lam "eza -AlH --icons -s modified --time-style long-iso"
 abbr -a che "chezmoi"
 # fzf modified file in current directory and merge it
 abbr -a chem "chezmoi merge (chezmoi --path-style absolute status . | awk '{print \$2}' | fzf)"

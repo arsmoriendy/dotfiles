296aaeb4fd52175218e169f5ae351351cbcd48d4
arsmoriendy <nugrahasteven@gmail.com>
2024-08-14 11:45:41 +0700

    fix(fish): eza abbrs is set to exa (old name)

--- dot_config/private_fish/conf.d/80-abbrs.fish
+++ dot_config/private_fish/conf.d/80-abbrs.fish
@@ -1,8 +1,8 @@
 abbr -a p "sudo $PACK_MAN"
 abbr -a sctl "sudo systemctl"
-abbr -a ls "exa --icons"
-abbr -a la "exa -alH --icons -F"
-abbr -a lam "exa -alH -s modified --time-style long-iso --icons -F"
+abbr -a ls "eza --icons"
+abbr -a la "eza -alH --icons -F"
+abbr -a lam "eza -alH -s modified --time-style long-iso --icons -F"
 abbr -a che "chezmoi"
 # fzf modified file in current directory and merge it
 abbr -a chem "chezmoi merge (chezmoi --path-style absolute status . | awk '{print \$2}' | fzf)"

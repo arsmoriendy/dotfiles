b26cbcf3c94a2cd175fc506ff3c76c4abb24d16e
arsmoriendy <nugrahasteven@gmail.com>
2024-08-28 16:07:11 +0700

    feat(fish): add SSH_AUTH_SOCK env var
    WARNING: this is only tested to work on nixos, according to the arch
    wiki; SSH_AUTH_SOCK should be set as `$XDG_RUNTIME_DIR/ssh-agent.socket`, it
    is currently set as `$XDG_RUNTIME_DIR/ssh-agent`.

--- dot_config/private_fish/conf.d/20-evars.fish
+++ dot_config/private_fish/conf.d/20-evars.fish
@@ -2,3 +2,4 @@ set -x TERMINAL wezterm # used for i3-sensible-terminal
 set -x EDITOR nvim
 set -x MANPAGER "nvim +Man!"
 set -x GOPATH "$HOME/.go"
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent"

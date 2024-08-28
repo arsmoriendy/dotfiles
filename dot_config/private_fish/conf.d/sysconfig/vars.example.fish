b1b3340786093996663ee75545f4bfb771f43363
arsmoriendy <nugrahasteven@gmail.com>
2024-08-28 17:10:47 +0700

    docs(fish): instruct to copy instead of rename vars.example.fish

--- dot_config/private_fish/conf.d/sysconfig/vars.example.fish
+++ dot_config/private_fish/conf.d/sysconfig/vars.example.fish
@@ -1,6 +1,6 @@
 # INFO: This is just an example/template file.
 # Each variable listed here is required to be specified.
-# Modify the variables and rename this file to `vars.fish` to make sure fish runs as expected
+# Modify the variables and copy this file to `./vars.fish` to make sure fish runs as expected
 
 # distro variables
 set -g DISTRO_ICON ""

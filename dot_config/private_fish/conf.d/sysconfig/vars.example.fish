a27e21aee7b6ec1c48c3fb009fe02c2b95f1daaf
arsmoriendy <nugrahasteven@gmail.com>
2024-08-28 17:06:18 +0700

    refactor(fish): replace sysconfig's vars.fish with vars.example.fish

---
+++ dot_config/private_fish/conf.d/sysconfig/vars.example.fish
@@ -0,0 +1,7 @@
+# INFO: This is just an example/template file.
+# Each variable listed here is required to be specified.
+# Modify the variables and rename this file to `vars.fish` to make sure fish runs as expected
+
+# distro variables
+set -g DISTRO_ICON ""
+set -g PACK_MAN "sudo pacman" # distro package manager
--- dot_config/private_fish/conf.d/sysconfig/vars.fish
+++
@@ -1,3 +0,0 @@
-# distro variables
-set -g DISTRO_ICON ""
-set -g PACK_MAN "sudo pacman" # distro package manager

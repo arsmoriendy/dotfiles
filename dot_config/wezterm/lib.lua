392c93a08a8892147a1237cccb55ed30eff28510
arsmoriendy <nugrahasteven@gmail.com>
2024-09-06 12:04:55 +0700

    feat(wezterm): add lib.list_extend function

--- dot_config/wezterm/lib.lua
+++ dot_config/wezterm/lib.lua
@@ -48,4 +48,22 @@ lib.tbl_extend = function(behavior, ...)
   return rtbl
 end
 
+--- Extends a list-like table with the values of another list-like table.
+--- (clone of nvim's function by the same name)
+---
+--- NOTE: This mutates dst!
+---
+---@generic T: table
+---@param dst T List which will be modified and appended to
+---@param src table List from which values will be inserted
+---@param start integer? Start index on src. Defaults to 1
+---@param finish integer? Final index on src. Defaults to `#src`
+---@return T dst
+function lib.list_extend(dst, src, start, finish)
+  for i = start or 1, finish or #src do
+    table.insert(dst, src[i])
+  end
+  return dst
+end
+
 return lib

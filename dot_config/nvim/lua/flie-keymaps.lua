cd6397aa2aa0e91765273f9204a3c1933bc140ee
arsmoriendy <nugrahasteven@gmail.com>
2024-10-26 14:30:46 +0700

    feat(nvim): check ConTeXt file types

--- dot_config/nvim/lua/flie-keymaps.lua
+++ dot_config/nvim/lua/flie-keymaps.lua
@@ -5,16 +5,29 @@ local kms = lib.kms
 --- ConTeXt main file name
 ---@type string
 local ctx_main_file
+---ConTeXt file types
+---@type table<string, string>
+local ctx_file_types = {
+  comp = "component",
+  env = "environment",
+}
 ---Returns wether a ConTeXt file is a main file (i.e. not an environment, component, etc).
 ---Does not check for file extension.
 ---@param ctx_fn string A ConTeXt filename (not path)
 ---@return boolean
 local function is_ctx_main(ctx_fn)
-  -- TODO: check after "_"
   local splitidx = lib.strrfindc(ctx_fn, '_')
   if splitidx == 0 then
     return true
   end
+
+  local dotidx = lib.strrfindc(ctx_fn, '.')
+  local type = ctx_fn:sub(underscoreidx + 1, dotidx - 1)
+
+  if ctx_file_types[type] == nil then
+    vim.notify("Unrecognized ConTeXt file type", vim.log.levels.ERROR)
+  end
+
   return false
 end
 cau("FileType", {

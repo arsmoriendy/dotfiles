1d626ed7df16f4ad90a1a0825aa610f76c527918
arsmoriendy <nugrahasteven@gmail.com>
2024-10-26 14:35:13 +0700

    fix(nvim): file-keymaps.lua typo

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -114,7 +114,7 @@ vim.diagnostic.config({
 -- load config from different files [
 require("functions")
 require("keymaps")
-require("flie-keymaps")
+require("file-keymaps")
 require("lazy").setup(
   "plugins", -- import plugins (:h lazy.nvim-lazy.nvim-structuring-your-plugins)
   {

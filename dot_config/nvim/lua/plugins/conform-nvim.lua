7ac8560300c9977d9631b9b0c070ff792b6ad58d
arsmoriendy <nugrahasteven@gmail.com>
2024-10-27 19:15:42 +0700

    feat(nvim): conform.nvim local and global toggle

--- dot_config/nvim/lua/plugins/conform-nvim.lua
+++ dot_config/nvim/lua/plugins/conform-nvim.lua
@@ -15,27 +15,84 @@ local formatters_by_ft = {
   lua = { "stylua" },
 }
 
-local format_on_save = true
-local toggle_format_on_save = function()
-  format_on_save = not format_on_save
-  vim.notify("Format on save: " .. tostring(format_on_save))
+-- global format on save {
+---@type boolean
+vim.g.format_on_save = true
+
+---@return boolean
+local function getg()
+  return vim.g.format_on_save
 end
+---
+---@param state boolean
+local function setg(state)
+  vim.g.format_on_save = state
+end
+
+local function toggle_g_format_on_save_wrapper()
+  -- toggle global format on save
+  setg(not getg())
+
+  local msg = string.format("%s formatting on save globally", getg() and "Enabled" or "Disabled")
+  vim.notify(msg)
+end
+-- }
+
+-- buffer format on save {
+---@return boolean|nil
+local function gets(bufn)
+  return vim.b[bufn].format_on_save
+end
+
+---@param bufn number
+---@param state boolean
+local function sets(bufn, state)
+  vim.b[bufn].format_on_save = state
+end
+
+local function toggle_current_b_format_on_save()
+  local current_bufn = vim.api.nvim_get_current_buf()
+
+  -- init or toggle
+  if gets(current_bufn) == nil then
+    sets(current_bufn, false)
+  else
+    sets(
+      current_bufn,
+      ---@diagnostic disable-next-line:param-type-mismatch
+      not gets(current_bufn)
+    )
+  end
+
+  local msg = string.format("%s formatting on save in current file", gets(current_bufn) and "Enabled" or "Disabled")
+  vim.notify(msg)
+end
+
+---@return conform.FormatOpts|nil
+local function format_on_save(bufn)
+  local b = gets(bufn)
+  if b ~= nil then
+    return b and {} or nil
+  end
+
+  return getg() and {} or nil
+end
+-- }
 
 local function config()
   local conform = require("conform")
 
   conform.setup({
     formatters_by_ft = formatters_by_ft,
-    format_on_save = function()
-      return format_on_save and {} or nil
-    end,
+    format_on_save = format_on_save,
     default_format_opts = {
       lsp_format = "fallback",
     },
   })
 
   lib.kms("n", "<Leader>i", conform.format, "Format file/buffer")
-  lib.kms("n", "<Leader>I", toggle_format_on_save, "Toggle format on save")
+  lib.kms("n", "<Leader>I", toggle_g_format_on_save_wrapper, "Toggle formatting on save globally")
+  lib.kms("n", "<Leader>Ib", toggle_current_b_format_on_save, "Toggle formatting on save in current file")
 end
 
 return {

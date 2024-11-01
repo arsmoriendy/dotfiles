9e6feb7cc8ac4d61c0bc0d9f2eaf9c8081ba979a
arsmoriendy <nugrahasteven@gmail.com>
2024-11-01 11:17:08 +0700

    feat(nvim): zenmode: set width and disable twilight

--- dot_config/nvim/lua/plugins/zen-mode-nvim.lua
+++ dot_config/nvim/lua/plugins/zen-mode-nvim.lua
@@ -1,4 +1,13 @@
+local opts = {
+  window = {
+    width = 80,
+  },
+  plugins = {
+    twilight = { enabled = false },
+  },
+}
 return {
   "folke/zen-mode.nvim",
   event = "VeryLazy",
+  opts = opts,
 }

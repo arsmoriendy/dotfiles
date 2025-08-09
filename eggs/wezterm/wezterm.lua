22aa0ddd1e2cdaddf035b085fe492a306b8bd536
arsmoriendy <nugrahasteven@gmail.com>
2025-08-09 16:11:43 +0700

    chore(wezterm): set universal default_prog to nu

--- eggs/wezterm/wezterm.lua
+++ eggs/wezterm/wezterm.lua
@@ -54,9 +54,7 @@ local config = {
     saturation = 0.9,
     brightness = 0.5,
   },
-  -- {% if SYSTEM.platform == "Windows" %}
-  --<yolk> default_prog = { "nu" },
-  -- {% end %}
+  default_prog = { "nu" },
 }
 
 -- This function returns the suggested title for a tab.

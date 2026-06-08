4cd3bc54a99b63f0fa494610cf24b3c3aee07a89
arsmoriendy <nugrahasteven@gmail.com>
2026-06-08 17:03:32 +0700

    fix(nvim): start treesitter

--- eggs/nvim/lua/plugins/nvim-treesitter.lua
+++ eggs/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
